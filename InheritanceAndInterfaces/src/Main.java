2303ce06fc25c5b48f8da61ab77839fc9f90dbe1
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-13 17:23:24 -0600

    Examining Object Base Class Inherited Methods

--- InheritanceAndInterfaces/src/Main.java
+++ InheritanceAndInterfaces/src/Main.java
@@ -1,20 +1,40 @@
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
 
-        Object object = new Object();
+        // NOTE: The constructor is deprecated but can still be used
+        Date date = new Date("5/11/2021");
 
-        System.out.println("String representation: " + object.toString());
-        System.out.println("Class: " + object.getClass());
-        System.out.println("Hash code: " + object.hashCode());
+        System.out.println("String representation: " + date.toString());
+        System.out.println("Class: " + date.getClass());
+        System.out.println("Hash code: " + date.hashCode());
 
-        Object anotherObject = new Object();
+        Date anotherDate = new Date("7/21/2021");
 
-        System.out.println("\nString representation of anotherObject: " + anotherObject.toString());
-        System.out.println("Are they equal?: " + object.equals(anotherObject));
+        System.out.println("\nString representation of another date: " + anotherDate.toString());
+        System.out.println("Are they equal?: " + date.equals(anotherDate));
 
-        Object objectCopy = object;
+        Date dateCopy = date;
 
-        System.out.println("\nString representation of copy: " + objectCopy.toString() + "\n");
-        System.out.println("Is the copy equal to the original?: " + object.equals(objectCopy));
+        System.out.println("\nString representation of copy: " + dateCopy.toString());
+        System.out.println("Is the copy equal to the original?: " + date.equals(dateCopy));
+
+//        String string = new String("A String");
+//
+//        System.out.println("String representation: " + string.toString());
+//        System.out.println("Class: " + string.getClass());
+//        System.out.println("Hash code: " + string.hashCode());
+//
+//        String anotherString = new String("Another string");
+//
+//        System.out.println("\nString representation of another string: " + anotherString.toString());
+//        System.out.println("Are they equal?: " + string.equals(anotherString));
+//
+//        String stringCopy = string;
+//
+//        System.out.println("\nString representation of copy: " + stringCopy.toString());
+//        System.out.println("Is the copy equal to the original?: " + string.equals(stringCopy));
     }
 }
