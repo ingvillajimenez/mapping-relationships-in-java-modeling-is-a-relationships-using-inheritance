e563fc274e75842c62a1404b675879c4e8b29048
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-13 21:08:31 -0600

    Instantiating Objects of Base and Derived Classes

--- InheritanceAndInterfaces/src/com/skillsoft/inheritanceandinterfaces/Book.java
+++ InheritanceAndInterfaces/src/com/skillsoft/inheritanceandinterfaces/Book.java
@@ -1,4 +1,18 @@
 package com.skillsoft.inheritanceandinterfaces;
 
-public class Book extends Object {
+public class Book {
+
+    private String bindingType = "Paperback";
+
+    public String getBindingType() {
+        return bindingType;
+    }
+
+    public void setBindingType(String bindingType) {
+        this.bindingType = bindingType;
+    }
+
+    public void checkOutBook() {
+        System.out.println("The book has been checked out successfully");
+    }
 }
--- InheritanceAndInterfaces/src/com/skillsoft/inheritanceandinterfaces/Main.java
+++ InheritanceAndInterfaces/src/com/skillsoft/inheritanceandinterfaces/Main.java
@@ -4,44 +4,35 @@ public class Main {
     public static void main(String[] args) {
 
         Book headFirstJava = new Book();
+        System.out.println("headFirstJava binding type: " + headFirstJava.getBindingType());
+        headFirstJava.checkOutBook();
+
+        System.out.println();
+
         Book effectiveJava = new Book();
-        Book javaCookbook = new Book();
-
-        System.out.println("Is headFirstJava an instance of Book Class?: " + (headFirstJava instanceof Book));
-        System.out.println("Is effectiveJava an instance of Book Class?: " + (effectiveJava instanceof Book));
-        System.out.println("Is javaCookbook an instance of Book Class?: " + (javaCookbook instanceof Book));
-
-        System.out.println();
-
-        System.out.println("Is headFirstJava an instance of Object Class?: " + (headFirstJava instanceof Object));
-        System.out.println("Is effectiveJava an instance of Object Class?: " + (effectiveJava instanceof Object));
-        System.out.println("Is javaCookbook an instance of Object Class?: " + (javaCookbook instanceof Object));
-
-        System.out.println();
-
-        Object headFirstJavaObject = headFirstJava;
-        Object effectiveJavaObject = effectiveJava;
-        Object javaCookbookObject = javaCookbook;
-
-        System.out.println("Is headFirstJavaObject an instance of Book Class?: " +
-                (headFirstJavaObject instanceof Book));
-        System.out.println("Is effectiveJavaObject an instance of Book Class?: " +
-                (effectiveJavaObject instanceof Book));
-        System.out.println("Is javaCookObject an instance of Book Class?: " +
-                (javaCookbookObject instanceof Book));
+        System.out.println("effectiveJava binding type: " + effectiveJava.getBindingType());
+        effectiveJava.checkOutBook();
 
         System.out.println();
 
         System.out.println("headFirstJava's class: " + headFirstJava.getClass());
         System.out.println("effectiveJava's class: " + effectiveJava.getClass());
-        System.out.println("javaCookbook's class: " + javaCookbook.getClass());
 
         System.out.println();
+
+        Object headFirstJavaObject = headFirstJava;
+        Object effectiveJavaObject = effectiveJava;
 
         System.out.println("headFirstJavaObject's class: " + headFirstJavaObject.getClass());
         System.out.println("effectiveJavaObject's class: " + effectiveJavaObject.getClass());
-        System.out.println("javaCookbookObject's class: " + javaCookbookObject.getClass());
 
         System.out.println();
+
+        System.out.println("headFirstJava binding type: " + headFirstJavaObject.getBindingType());
+        headFirstJavaObject.checkOutBook();
+
+        System.out.println("effectiveJava binding type: " + effectiveJavaObject.getBindingType());
+        effectiveJavaObject.checkOutBook();
+
     }
 }
